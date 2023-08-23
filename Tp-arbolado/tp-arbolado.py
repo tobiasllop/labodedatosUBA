2221e4c4973c1b0190d61f9c2acc29ba1ffea010
Tobias Llop <tobiasllop@gmail.com>
2023-08-23 17:00:01 -0300

    Add files via upload

--- Tp-arbolado/tp-arbolado.py
+++ Tp-arbolado/tp-arbolado.py
@@ -21,4 +21,30 @@ print(res)
 
 #2
 def especies(lista_arboles):
-        res = []
+        res = set()
+        for arbol in lista_arboles:
+                res.add(arbol["nombre_com"])
+        return res
+#print(especies(leer_parque(nombre_archivo, "GENERAL PAZ")))
+
+#3
+def contar_ejemplares(lista_arboles):
+        res = {}
+        for arbol in lista_arboles:
+                if arbol["nombre_com"] in res:
+                        res[arbol["nombre_com"]] += 1
+                else:
+                      res[arbol["nombre_com"]] = 1
+        return res
+
+print(contar_ejemplares(leer_parque(nombre_archivo, "GENERAL PAZ"))["Jacarandá"])
+
+#5
+def obtener_inclinaciones(lista_arboles, especie):
+    res = []
+    for arbol in lista_arboles:
+        if arbol["nombre_com"] == especie:
+            res.append(arbol["inclinacio"])
+    return res
+
+print(obtener_inclinaciones(leer_parque(nombre_archivo, "GENERAL PAZ"), "Jacarandá"))
